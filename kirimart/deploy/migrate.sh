7962e5ccbed65b35caf0f08de958b31446bdaac1
RedyPutraSembada <redyputra120503@gmail.com>
2026-06-10 11:21:19 +0700

    update bash migrate

--- kirimart/deploy/migrate.sh
+++ kirimart/deploy/migrate.sh
@@ -5,7 +5,7 @@ echo "Menjalankan migrasi database (drizzle-kit push)..."
 docker run --rm -it \
   --network kawanbelanja-net \
   --env-file .env.production \
-  kb-nextjs \
+  6-nextjs-nextjs \
   bun run db:push
 
 echo "Migrasi selesai!"
